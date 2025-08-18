82946de60172d60a643366070e737c72ae2b9292
VaidehiCodes <vpshah2107@gmail.com>
2025-08-18 20:40:20 +0530

    hallo

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import adminRoutes from "./routes/adminRoute.js";
+import authRoutes from "./routes/authRoute.js";
+import songRoutes from "./routes/songRoute.js";
+import albumRoutes from "./routes/albumRoute.js";
+import statsRoutes from "./routes/statsRoute.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/song", songRoutes);
+app.use("/api/album", albumRoutes);
+app.use("/api/stats", statsRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
