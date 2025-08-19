b8c0d90caaa233137201f0d9b7cf174c51eac30a
VaidehiCodes <vpshah2107@gmail.com>
2025-08-19 19:19:58 +0530

    push1:  add eg routes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("User route is working");
+  res.send("Admin route is working");
 });
 
 export default router;
