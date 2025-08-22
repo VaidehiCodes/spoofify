e662f2e4fadee415076a1a4dc0ddcb5bf1aa3155
VaidehiCodes <vpshah2107@gmail.com>
2025-08-22 18:01:44 +0530

    bad models yayyyy

--- backend/src/models/albumModel.js
+++ backend/src/models/albumModel.js
@@ -1,25 +1,32 @@
+import { time } from "console";
 import mongoose from "mongoose";
+import { ref } from "process";
 
-const albumSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    artist: {
-      type: String,
-      required: true,
-    },
-    releaseDate: {
-      type: Date,
-      required: true,
-    },
-    coverImageUrl: {
-      type: String,
-      required: true,
-    },
+const albumSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
-
+  artist: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  ImageUrl: {
+    type: String,
+    required: true,
+  },
+  songs: [
+    {
+      title: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    },
+  ],
+  timestamps: true,
+});
 export const Album = mongoose.model("Album", albumSchema);
