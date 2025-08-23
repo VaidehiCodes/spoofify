da29970282f0bb030929826f7681e757c70bf9f3
VaidehiCodes <vpshah2107@gmail.com>
2025-08-23 19:49:19 +0530

    check

--- backend/src/routes/adminRoute.js
+++ backend/src/routes/adminRoute.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import { getAdmin } from "../controller/adminController";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Admin route is working");
-});
+router.get("/", getAdmin);
 
 export default router;
